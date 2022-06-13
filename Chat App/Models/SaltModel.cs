35621ef6aad346fabf30559ff21aa78babf1f526
Hakan Sahin <Hakan.sahin994@gmail.com>
2022-06-13 09:54:44 +0200

    Minor updates regarding Encryption

---
+++ Chat App/Models/SaltModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Chat_App.Models
+{
+    public class SaltModel
+    {
+        public SaltModel(string pass, string salt)
+        {
+            Pass = pass;
+            Salt = salt;
+        }
+        public string Pass { get; set; }
+        public string Salt { get; set; }
+    }
+}
